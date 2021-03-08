a75a0a567f81fde9fb56270fb534916fb457fede
Tissinapa <80274382+Tissinapa@users.noreply.github.com>
2021-03-08 18:45:02 +0200

    Tehtavat

---
+++ app/src/main/java/com/example/viikko7harjoitustehtvt/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.viikko7harjoitustehtvt;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView text;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        text = (TextView) findViewById(R.id.textView);
+    }
+
+    public void function(View v) {
+        System.out.println("Hello world!");
+        text.setText("Hello world!");
+
+    }
+}
